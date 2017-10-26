fa0ebb8ed72964e9fafea88e8929e676609cba14
subhodi <subhodpoojary@gmail.com>
2017-10-26 17:33:43 +0530

    docker-compose.yml file genrate | start peers

--- peers/router.js
+++ peers/router.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('./controller');
+const peer = require('./peer-setup');
 
 router.post('/', controller.createPeer);
 
@@ -15,5 +16,9 @@ router.post('/yaml-file', controller.createYamlFile);
 
 // specify `?profileName`
 router.post('/genesis-block', controller.createGenesisBlock);
+
+router.post('/create-peer', peer.createPeer);
+
+router.post('/generate-file', peer.generateFile);
 
 module.exports = router;
