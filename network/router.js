3f37464a5ea145ee5c91412fe4adef487e7c7401
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-10-27 14:00:03 +0530

    Add CORS

--- network/router.js
+++ network/router.js
@@ -5,6 +5,17 @@ const router = express.Router();
 
 const controller = require('./controller');
 
+// middleware to allow CORS
+router.use((req, res, next) => {
+  var origin = req.headers.origin;
+
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 router.post('/', controller.createNetwork);
 
 // specify `?channel` & `?profileName`
